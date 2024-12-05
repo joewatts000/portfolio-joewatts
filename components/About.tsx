ac46809415ce36d14c7f73e5172d0606380a2926
joewatts000 <squarepeoplesolutions@gmail.com>
2024-12-05 06:42:02 +0000

    text

--- components/About.tsx
+++ components/About.tsx
@@ -12,8 +12,8 @@ export function About() {
             <h2 className="text-3xl font-bold mb-8">About Me</h2>
             <p className="text-lg text-muted-foreground mb-8">
               I&apos;m a passionate full-stack developer with over 10 years experience in web technologies. With a strong foundation in frontend development and a growing experience in backend development,
-              I specialize in building scalable applications using React, Typescript and Node but I also love experimenting with any library or framework</p>
-            <p className="text-lg text-muted-foreground mb-8">I have 6 years field and technical experience and 8 years software development experience for a leading market research company building web apps and research and analytics tools</p>
+              I specialize in React, Typescript and Node but I also love experimenting with any library or framework</p>
+            <p className="text-lg text-muted-foreground mb-8">I have 6 years field and technical experience and 8 years software development experience for a leading market research company, building web apps and research & analytics tools</p>
             <p className="text-lg text-muted-foreground mb-8">During that time i also worked for digital design agencies building games, websites and apps, using custom CMS&apos;s or wordpress with custom themes. And have built my own apps and sites with a variety of technologies</p>
             <TechStack />
             <div className="flex justify-center gap-4">
